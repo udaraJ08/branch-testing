39b7a5c84ae37d14146695becfa579c2aa86019c
USER <udaraj08@gmail.com>
2021-08-18 10:55:48 +0530

    checking branches

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import SplashScreen from './public/components/screens/SplashScreen';
+
+import '../src/public/css/normalize.css'
+
+function App() {
+  return (
+    <div className="main-container">
+      <SplashScreen />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/public/components/screens/SplashScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+//importing css
+import '../../css/normalize.css'
+import '../../css/splashScreen.css'
+
+export class SplashScreen extends Component {
+    render() {
+        return (
+            <div className="splash-container">
+
+            </div>
+        )
+    }
+}
+
+export default SplashScreen
